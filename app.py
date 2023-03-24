b8450ae07fd7a7cda8f8572cb0dfd29aff8c4e1c
ralph-p <mr.pereira91@gmail.com>
2023-03-24 15:46:18 -0400

    added requirements, added procfile

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, Response, jsonify, json, request
 from sb_controller import find_all_profiles, login_user, get_user, get_task_list
 from utils import get_token
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return jsonify({
         'greeting': 'Welcome to Habit Tracker Pro'
     })
-@app.route('/login',  methods=['POST'])
+@application.route('/login',  methods=['POST'])
 def login():
     data = request.get_json()
     try:
@@ -19,26 +19,26 @@ def login():
         }, 201)
     except Exception as e:
         print(e)
-        return Response('''{"message": "Bad Request"}''', status=400, mimetype='application/json')
+        return Response('''{"message": "Bad Request"}''', status=400, mimetype='applicationlication/json')
 
-@app.route('/user')
+@application.route('/user')
 def user():
     token = get_token(request)
     return jsonify({
         'user': get_user(token),
     })
 
-@app.route('/profiles')
+@application.route('/profiles')
 def profiles():
     return jsonify({
         'profiles': find_all_profiles(),
     })
 
-@app.route('/tasks')
+@application.route('/tasks')
 def tasks():
     token = get_token(request)
     return jsonify({
         'tasks': get_task_list(token),
     })
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = True)
+    application.run(host='0.0.0.0', debug = True)
